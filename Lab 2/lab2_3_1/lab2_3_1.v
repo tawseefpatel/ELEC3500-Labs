715ae793865803c06e5ccfd4375b0cb3e61b27a6
Tawseef Patel <63173329+tawseefpatel@users.noreply.github.com>
2022-10-19 13:37:38 -0400

    test

--- Lab 2/lab2_3_1/lab2_3_1.v
+++ Lab 2/lab2_3_1/lab2_3_1.v
@@ -9,21 +9,12 @@ module moduleName (
     output cout
 );
 
-xor (s[0], a[0], b[0], cin);
-and (fa1_out1, a[0], b[0]), (fa1_out2, a[0], cin), (fa1_out3, cin, b[0]) ;
-or (cout1, fa1_out1, fa1_out2, fa1_out3);
+xor (s[0], a[0], b[0], cin), (s[1], a[1], b[1], cout1), (s[2], a[2], b[2], cout2), (s[3], a[3], b[3], cout3) ;
+and (fa1_out1, a[0], b[0]), (fa1_out2, a[0], cin), (fa1_out3, cin, b[0]), (fa2_out1, a[1], b[1]), (fa2_out2, a[1], cout1), (fa2_out3, cout1, b[1]), (fa3_out1, a[2], b[2]), (fa3_out2, a[2], cout2), (fa3_out3, cout2, b[2]), (fa4_out1, a[3], b[3]), (fa4_out2, a[3], cout3), (fa4_out3, cout3, b[3])   ;
+or (cout1, fa1_out1, fa1_out2, fa1_out3), (cout2, fa2_out1, fa2_out2, fa2_out3), (cout3, fa3_out1, fa3_out2, fa3_out3),(cout, fa4_out1, fa4_out2, fa4_out3) ;
 
-xor (s[1], a[1], b[1], cout1);
-and (fa2_out1, a[1], b[1]), (fa2_out2, a[1], cout1), (fa2_out3, cout1, b[0]);
-or (cout2, fa2_out1, fa2_out2, fa2_out3);
 
-xor (s[2], a[2], b[2], cout2);
-and (fa3_out1, a[2], b[2]), (fa3_out2, a[2], cout2), (fa3_out3, cout2, b[2]);
-or (cout3, fa3_out1, fa3_out2, fa3_out3);
 
-xor (s[3], a[3], b[3], cout3);
-and (fa4_out1, a[3], b[3]), (fa4_out2, a[3], cout3), (fa4_out3, cout3, b[3]);
-or (cout, fa4_out1, fa4_out2, fa4_out3);
 
 
     
